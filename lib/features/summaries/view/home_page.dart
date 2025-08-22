93b711b79deea828cdd43a18af9727fc3c89ba57
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2025-08-22 20:02:19 +0200

    Adding tab bar on summary page

--- lib/features/summaries/view/home_page.dart
+++ lib/features/summaries/view/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/app/shared/components/app_bar.dart';
 import 'package:expense_tracker/app/shared/util/toast_service.dart';
+import 'package:expense_tracker/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,18 +9,41 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          OutlinedButton(
-            onPressed: () => ToastService.showInfoToast(
-              context,
-              "This is a regular snackbar",
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: _createAppBar(),
+        body: Column(
+          children: [
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () => ToastService.showInfoToast(
+                context,
+                "This is a regular snackbar",
+              ),
+              child: Text("Update worked!"),
             ),
-            child: Text("Update worked!"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _createAppBar() {
+    return AppBar(
+      toolbarHeight: 40,
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TabBar(
+              tabs: [
+                Tab(child: Icon(Icons.bar_chart)),
+                Tab(child: Icon(Icons.area_chart_rounded)),
+                Tab(child: Icon(Icons.person))
+              ]
           ),
-        ],
+        ),
       ),
     );
   }
