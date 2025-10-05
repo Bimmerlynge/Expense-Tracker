cd7cfcba09fbe8a0e57cc49d25292592536155ea
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2025-10-05 15:25:25 +0200

    Remove old transaction service

--- lib/app/providers/app_providers.dart
+++ lib/app/providers/app_providers.dart
@@ -1,10 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 import 'package:expense_tracker/domain/category.dart';
 import 'package:expense_tracker/domain/person.dart';
-import 'package:expense_tracker/domain/transaction.dart';
 import 'package:expense_tracker/features/categories/service/category_firebase_service.dart';
 import 'package:expense_tracker/features/transactions/providers/add_transaction_providers.dart';
-import 'package:expense_tracker/features/transactions/service/transaction_firebase_service.dart';
 import 'package:expense_tracker/features/users/service/user_firestore_service.dart';
 import 'package:expense_tracker/features/users/view_model/user_view_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -30,12 +28,6 @@ final userViewModelProvider = Provider<UserViewModel>((ref) {
   final userFirestoreService = ref.watch(userFirestoreServiceProvider);
   return UserViewModel(userFirestoreService);
 });
-
-final transactionFirestoreServiceProvider =
-    Provider<TransactionFirebaseService>((ref) {
-      final firestore = ref.read(firestoreProvider);
-      return TransactionFirebaseService(firestore, ref);
-    });
 
 final categoryFirebaseServiceProvider = Provider<CategoryFirebaseService>((
   ref,
@@ -85,12 +77,4 @@ final transactionRangeProvider = StateProvider<DateTimeRange>((ref) {
       start: DateTime(now.year, now.month, 1),
       end: DateTime(now.year, now.month + 1, 1)
   );
-});
-
-final transactionStreamInRangeProvider = StreamProvider<List<Transaction>>((
-  ref,
-) {
-  final range = ref.watch(transactionRangeProvider);
-  final service = ref.read(transactionFirestoreServiceProvider);
-  return service.getTransactionStreamInRange(range.start, range.end);
 });
